0107bd886891c2948748ebc6e5b610f0f1dd5286
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2022-02-15 16:07:01 +0200

    fix env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
-const dotenv = require('dotenv');
 const withImages = require('next-images');
-const env = dotenv.config().parsed;
 
 module.exports = {
-  env,
   webpack: (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
-  
+
     return config;
-  }
+  },
+  env: {
+    MONGO_URL: 'mongodb+srv://root:root@cluster0.ullsi.mongodb.net/money-management?retryWrites=true&w=majority',
+    NEXT_PUBLIC_BASE_URL: 'http://localhost:3000/api',
+    MAIL_ID: 'seredenko.bohdan@chnu.edu.ua',
+    MAIL_PASSWORD: '135seredenko135',
+  },
 };
 
 module.exports = withImages();

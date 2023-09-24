b30c111278f0f3a629cd279abb8b71808dab03da
Bogdan <bohdan@omnifi.io>
2023-09-24 10:05:11 +0300

    push env

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ module.exports = {
 
     return config;
   },
-  env: {
-    MONGO_URL: 'mongodb+srv://root:root@cluster0.ullsi.mongodb.net/money-management?retryWrites=true&w=majority',
-    NEXT_PUBLIC_BASE_URL: 'http://localhost:3000/api',
-    MAIL_ID: 'seredenko.bohdan@chnu.edu.ua',
-    MAIL_PASSWORD: '135seredenko135',
-  },
 };
 
 module.exports = withImages();
